9e10ae36da7f7d407919d23cd99b43bcb19338a7
softprops <d.tangren@gmail.com>
2015-05-31 20:04:50 -0400

    err msg

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::io;
 
 fn main() {
   let mut client = Client::new();
-    let mut res =
+  // thread '<main>' panicked at 'called `Result::unwrap()` on an `Err` value: Ssl(OpenSslErrors([UnknownError { library: "SSL routines", function: "SSL23_GET_SERVER_HELLO", reason: "tlsv1 alert protocol version" }]))'
+  let mut res =
         client.get("https://www.howsmyssl.com/a/check").send().unwrap();
   io::copy(&mut res, &mut io::stdout()).unwrap();
 }
